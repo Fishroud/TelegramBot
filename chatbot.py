be52c378a4357e4b24a9f17c1d4eee3c96644ef4
Fishroud <3334679655@qq.com>
2024-02-16 16:29:17 +0800

    add chatgpt function

--- chatbot.py
+++ chatbot.py
@@ -4,7 +4,16 @@ CallbackContext)
 import configparser
 import logging
 import redis
+from ChatGPT_HKBU import HKBU_ChatGPT
 global redis1
+
+def equiped_chatgpt(update, context):
+    global chatgpt
+    reply_message = chatgpt.submit(update.message.text)
+    logging.info("Update: " + str(update))
+    logging.info("context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
 def main():
     # Load your token and create an Updater for your Bot
     config = configparser.ConfigParser()
@@ -12,6 +21,7 @@ def main():
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
     global redis1
+    global chatgpt
     redis1 = redis.Redis(host=(config['REDIS']['HOST']),
     password=(config['REDIS']['PASSWORD']),
     port=(config['REDIS']['REDISPORT']))
@@ -19,11 +29,17 @@ def main():
     # and why things do not work as expected Meanwhile, update your config.ini as:
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     # register a dispatcher to handle message: here we register an echo dispatcher
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
+    #echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    #dispatcher.add_handler(echo_handler)
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("add", add))
     dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # dispatcher for chatgpt
+    chatgpt = HKBU_ChatGPT()
+    chatgpt_handler = MessageHandler(Filters.text & (~Filters.command), equiped_chatgpt)
+    dispatcher.add_handler(chatgpt_handler)
+
     # To start the bot:
     updater.start_polling()
     updater.idle()
@@ -35,6 +51,7 @@ def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
     # Define a few command handlers. These usually take the two arguments update and
     # context. Error handlers also receive the raised TelegramError object in error.
+    
 
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
